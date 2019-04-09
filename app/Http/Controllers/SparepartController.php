bcc9277ab3e350e15dee3159a30a48990c33f634
EENDRUU <christianandrew4321@gmail.com>
2019-04-10 00:27:46 +0700

    asdas

--- app/Http/Controllers/SparepartController.php
+++ app/Http/Controllers/SparepartController.php
@@ -12,11 +12,6 @@ class SparepartController extends Controller
 {
     protected $user;
 
-    public function __construct()
-    {
-        $this->user = JWTAuth::parseToken()->authenticate();
-    }
-
     public function index()
     {
         return Sparepart::all();
