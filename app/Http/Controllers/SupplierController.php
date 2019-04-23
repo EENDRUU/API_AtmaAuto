4f8b210891dd0500ee69da3a1d05334b1d645469
EENDRUU <christianandrew4321@gmail.com>
2019-04-23 16:59:45 +0700

    asd

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -75,7 +75,10 @@ class SupplierController extends Controller
             ]);
         }
         else{
-            return response()->json($suppliers,200);
+            return response()->json([
+                $suppliers,
+                'message' => 'success'
+                ]);
 
         }
     }
