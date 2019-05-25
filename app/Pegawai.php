a6b5a1e5d5e868825a6b59b31a3bfe0e0fb4a189
EENDRUU <christianandrew4321@gmail.com>
2019-05-25 22:51:19 +0700

    asd

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -9,7 +9,9 @@ class Pegawai extends Model
 
     protected $table = 'pegawai';
     protected $primaryKey = 'ID_PEGAWAI';
+    public $timestamps = false;
     public $incrementing = false;
+
 
     protected $fillable = [
         'NAMA_PEGAWAI','NOMORTELEPON_PEGAWAI',
